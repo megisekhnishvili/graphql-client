a87117ecb0aa951a043e26d2ef0f9e9fa94d125c
megisekhnishvili <megi.sekhniashvili.1@btu.edu.ge>
2024-10-03 19:59:52 +0400

    Initial commit of React GraphQL client

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./db.js"; 
+import { typeDefs } from "./schema.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    game: (_, { id }) => db.games.find((game) => game.id === id),
+    authors: () => db.authors,
+    author: (_, { id }) => db.authors.find((author) => author.id === id),
+    reviews: () => db.reviews,
+    review: (_, { id }) => db.reviews.find((review) => review.id === id),
+  },
+  Game: {
+    reviews: (parent) => db.reviews.filter((r) => r.game_id === parent.id),
+  },
+  Review: {
+    author: (parent) => db.authors.find((a) => a.id === parent.author_id),
+    game: (parent) => db.games.find((g) => g.id === parent.game_id),
+  },
+  Author: {
+    reviews: (parent) => db.reviews.filter((r) => r.author_id === parent.id),
+  },
+  Mutation: {
+    addGame: (_, { game }) => {
+      const newGame = { ...game, id: Math.random().toString() };
+      db.games.push(newGame);
+      return newGame;
+    },
+    deleteGame: (_, { id }) => {
+      db.games = db.games.filter((g) => g.id !== id);
+      return db.games;
+    },
+    updateGame: (_, { id, edits }) => {
+      db.games = db.games.map((g) => (g.id === id ? { ...g, ...edits } : g));
+      return db.games.find((g) => g.id === id);
+    },
+  },
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at: ${url}`);
